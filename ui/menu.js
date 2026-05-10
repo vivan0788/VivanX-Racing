0bfe8d2166fd26997bb9bf0002487da571f9bea7
Avinash Sahani <232133417+vivan0788@users.noreply.github.com>
2026-05-10 12:06:10 +0530

    Add Menu object with show and hide methods

---
+++ ui/menu.js
@@ -0,0 +1,12 @@
+const Menu = {
+
+    show() {
+        document.getElementById("menu").style.display = "block";
+        canvas.style.display = "none";
+    },
+
+    hide() {
+        document.getElementById("menu").style.display = "none";
+        canvas.style.display = "block";
+    }
+};
